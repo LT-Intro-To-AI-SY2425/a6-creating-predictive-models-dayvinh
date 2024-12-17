56ba047f42d553feb9baeed9054aca5c3b79cbb7
Davin Nguyen <davin.nguyen2017@gmail.com>
2024-12-17 10:08:37 -0600

    part 3 done

--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -9,14 +9,42 @@ x = data[["miles","age"]].values
 y = data["Price"].values
 
 #split the data into training and testing data
-
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
 #create linear regression model
-
+model = LinearRegression().fit(xtrain, ytrain)
 #Find and print the coefficients, intercept, and r squared values. 
 #Each should be rounded to two decimal places. 
+coef = np.around(model.coef_, 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = round(model.score(x, y),2)
 
+print(coef)
+print(intercept)
+print(r_squared)
 
 #Loop through the data and print out the predicted prices and the 
 #actual prices
+
 print("***************")
 print("Testing Results")
+
+predict = model.predict(xtest)
+predict = np.around(predict, 2)
+print(predict)
+
+print("\nPredictions for specific scenarios:")
+pOneMiles = 89000
+pOneAge = 10
+prediction = model.predict([[pOneMiles, pOneAge]])
+print(f"Predicted worth when miles is {pOneMiles} and age is {pOneAge}: {prediction}")
+
+pTwoMiles = 150000
+pTwoAge = 20
+predictionTwo = model.predict([[pTwoMiles, pTwoAge]])
+print(f"Predicted worth when miles is {pTwoMiles} and age is {pTwoAge}: {predictionTwo}")
+
+for index in range(len(xtest)):
+    actual = ytest[index] 
+    predicted_y = [index] 
+    x_coord = xtest[index] 
+    print(f"miles: {x_coord[0]} age: {x_coord[1]}  Actual: {actual} Predicted: {predicted_y}")
--- part3-multivariable-linear-regression/a6_part3_writeup.md
+++ part3-multivariable-linear-regression/a6_part3_writeup.md
@@ -5,9 +5,12 @@ After completing `a6_part3.py` answer the following questions
 ## Questions to answer
 
 1. What is the R Squared coefficient for your model? What does that mean about the model in relation to your data?
-
+The R Squared coefficient for my model 0.86 which means that the model is has a good amount of correlation with the data.
 2. Is your model accurate? Why or why not?
+My model is womewhat accurate as the predicted values it outputs sometimes are close to matching the actual values.
 
 3. What does the model predict a 10-year-old car with 89000 miles is worth? What about a car that is 20 years old with 150000 miles?
-
+10-year-old: 9180
+20-year-old: 2044
 4. You may notice that some of your predicted results are negative. This is occurring when the value of age and the mileage of the car are very high. Why do you think this is happening?
+I believe this is happening because the model is of a linear graph in which the line will keep going infinitely, so it would make sense that eventually the line reaches a negative value due to a the trend its following.
